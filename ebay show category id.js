c4c89846cc11a94e487f35b0d442c3cf80f9027b
janek49 <68037466+janek49@users.noreply.github.com>
2022-07-17 21:32:37 +0200

    Create ebay show category id.js

---
+++ ebay show category id.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         eBay show category Id
+// @namespace    http://jbcode.pl/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @include      *.ebay.*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ebay.de
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener('load', function () {
+    setTimeout(() => {
+
+        var bc = document.getElementById("bc");
+        var col = bc.getElementsByClassName("seo-breadcrumb-text");
+        var a = col[col.length-1];
+        var catIdArr = a.getAttribute("href").split("/");
+        var catId = catIdArr[catIdArr.length - 2];
+
+        var h2 = document.createElement("h2");
+        h2.innerText = "Category ID: " + catId;
+        document.getElementById("RightSummaryPanel").appendChild(h2);
+
+    }, 1000);
+}, false);
+
